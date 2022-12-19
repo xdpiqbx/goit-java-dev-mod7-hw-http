b9d65385153b421101c992f2b0b1bca1ea06c70c
xdpiqbx <xdpiqbx@gmail.com>
2022-12-19 10:21:20 +0200

    added todo

--- app/src/main/java/com/dpiqb/App.java
+++ app/src/main/java/com/dpiqb/App.java
@@ -3,6 +3,7 @@ package com.dpiqb;
 public class App {
     public static void main(String[] args) {
         HttpStatusImageDownloader httpStatusImageDownloader = new HttpStatusImageDownloader();
-        httpStatusImageDownloader.downloadStatusImage(200);
+        // TODO check if image exist, if exist do not download.
+        httpStatusImageDownloader.downloadStatusImage(203);
     }
 }
